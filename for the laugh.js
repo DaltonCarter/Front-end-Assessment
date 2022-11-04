700db8f992b685d79c965899f5fea70f47048aa3
Dalton Carter <DJ_Carter_Gottenbos@yahoo.com>
2022-11-04 14:51:01 -0700

    You guessed it more nitpick changes!

--- for the laugh.js
+++ for the laugh.js
@@ -4,7 +4,7 @@ console.log('Goliath Online')
 let jokebtn = document.querySelector('#joke')
 
 function jape() {
-    alert('RETICULATING SPLI... console.OOPS the splines have failed to reticulate! It seems they have fallen out of time and space! R.I.P  Look at the Kirby: ^(^-^)^   <("<)     (>")>')
+    alert('RETICULATING SPLI... console.OOPS the splines have failed to reticulate! It seems they have fallen out of time and space! R.I.P  Look at the Kirby: (^-^)   <("<)     (>")>')
     console.log('Totally Worth It!')
 }
 
